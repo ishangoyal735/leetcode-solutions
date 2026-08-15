f6992414d67980ac5ee4f5aa7344ba7e2117f855
Ishan Goyal <ishangoyal735@gmail.com>
2026-08-15 11:47:30 +0530

    Time: 0 ms (100%), Space: 26 MB (94.32%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,12 +1,39 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int ans=0;
-        for(auto  val:nums)
+    int singleNonDuplicate(vector<int>& A) {
+        int n = A.size();
+         if(n==1) 
+        return A[0];
+        int st =0;
+        int end= n-1;
+        int mid;
+       
+        while(st<=end)
         {
-            ans=ans ^ val;
+            mid = st + (end-st)/2;
+            if(mid == 0 &&  A[mid] != A[mid+1])
+                return A[mid];
+            if(mid == n-1 &&  A[mid] != A[mid-1])
+                return A[mid];
+                   
+            if(A[mid-1]!=A[mid] && A[mid]!=A[mid+1])
+                return A[mid];
+            if(mid % 2 == 0)// even number of elements each sid of mid
+           {
+             if(A[mid-1]==A[mid])
+               end = mid-1; 
+            else 
+               st = mid+1;
+           }
+           else  // odd numbers of elements each  side of mid 
+           {
+            if(A[mid-1]==A[mid])
+               st = mid+1;
+            else 
+               end = mid-1;
+           }
         }
-        return ans;
+        return -1;
         
     }
 };
